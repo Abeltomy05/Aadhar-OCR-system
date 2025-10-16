e508705e9cba933a15cd4e12b0732cc89b78a97c
Abel Tomy <abeltomy05@gmail.com>
2025-10-16 14:39:39 +0530

    Increased toast timing

--- client/src/hooks/useToast.hook.tsx
+++ client/src/hooks/useToast.hook.tsx
@@ -2,15 +2,15 @@ import toast from "react-hot-toast";
 
 export const useToast = ()=>{
     const success = (message: string) => {
-    toast.success(message, { duration: 3000 });
+    toast.success(message, { duration: 5000 });
   };
 
   const error = (message: string) => {
-    toast.error(message, { duration: 3000 });
+    toast.error(message, { duration: 5000 });
   };
 
   const info = (message: string) => {
-    toast(message, { duration: 3000 });
+    toast(message, { duration: 5000 });
   };
 
   return { success, error, info };
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,6 +8,6 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
     <Toaster
-      position='top-center' reverseOrder={false} toastOptions={{duration:3000, style:{  background: '#333',color: '#fff',borderRadius: '8px'}}}/>
+      position='top-center' reverseOrder={false} toastOptions={{duration:5000, style:{  background: '#333',color: '#fff',borderRadius: '8px'}}}/>
   </StrictMode>,
 )
