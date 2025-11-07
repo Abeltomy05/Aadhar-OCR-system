7d1b963b102d52540e749ecaaf6fc4192199e468
Abel Tomy <abeltomy05@gmail.com>
2025-11-07 09:29:33 +0530

    called env from cofig file

--- api/src/external/googleVision.ts
+++ api/src/external/googleVision.ts
@@ -1,4 +1,5 @@
 import vision from '@google-cloud/vision';
+import { config } from '../config/env';
 
-const credentials = JSON.parse(process.env.GOOGLE_CREDENTIALS_JSON!);
+const credentials = JSON.parse(config.GOOGLE_CREDENTIALS_JSON!);
 export const visionClient = new vision.ImageAnnotatorClient({ credentials });
